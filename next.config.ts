10b385c8f48dd7c3d1c7e31636d6e5ba8fd45d66
pradeepsuthar3 <pradeep.suthar@acuteinformatics.in>
2025-05-12 22:24:25 +0530

    changes in next.config

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,16 @@
 import type { NextConfig } from "next";
 
-const isGithubPages = process.env.DEPLOY_TARGET === "gh-pages";
-
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: isGithubPages ? "/my-portfolio-new" : "",
-  assetPrefix: isGithubPages ? "/my-portfolio-new/" : "",
+  basePath: "/my-portfolio-new",
+  assetPrefix: "/my-portfolio-new/",
   images: {
     unoptimized: true,
   },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
+      issuer: /\.[jt]sx?$/,
       use: ["@svgr/webpack"],
     });
     return config;
